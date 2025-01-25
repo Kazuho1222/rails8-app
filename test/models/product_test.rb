590d757df392362c4f3c402118a0bfdb43cf7c8e
Kazuho1222 <destiny5914@gmail.com>
2025-01-26 00:18:37 +0900

    メール送信をテストする

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,17 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  include ActionMailer::TestHelper
+
+  test "sends email notifications when back in stock" do
+    product = products(:tshirt)
+
+    # 製品を在庫切れにする
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
